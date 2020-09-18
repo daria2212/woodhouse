3fe522fe797d3ef61e9be724b3c65aaca0d75ccf
Daria <nabatova.darya@protonmail.com>
2020-09-18 16:09:01 +0300

    modal-form styles and script

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,4 @@
+// Menu button script
 const menuBtn = document.querySelector('.toggle')
 const menu = document.querySelector('.main-nav')
 
@@ -9,6 +10,8 @@ menuBtn.addEventListener('click', function (evt) {
     menu.classList.toggle('main-nav--opened')
 });
 
+// Slider script
+
 $('.header-slider').slick({
     infinite: false,
     dots: true,
@@ -16,4 +19,36 @@ $('.header-slider').slick({
     prevArrow: '<img class="left" src="images/left.svg" width="20" height="15">',
     nextArrow: '<img class="right" src="images/right.svg" width="20" height="15">',
     infinite: true
+});
+
+// Modal form script
+
+let popupBtn = document.querySelector('.header__button');
+let popup = document.querySelector('.modal-form');
+let overlay = document.querySelector('.modal-overlay');
+let close = document.querySelector('.modal-form__close');
+
+popupBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.add('modal-show');
+    overlay.classList.add('overlay-show');
+});
+
+close.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.remove('modal-show');
+    overlay.classList.remove('overlay-show');
+});
+
+overlay.addEventListener('click', function () {
+    popup.classList.remove('modal-show');
+    overlay.classList.remove('overlay-show');
+});
+
+window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        popup.classList.remove('modal-show');
+        overlay.classList.remove('overlay-show');
+    }
 });
